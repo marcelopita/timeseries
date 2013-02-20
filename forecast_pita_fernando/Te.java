ddb6805398c72fc79d09494f28b04a9361002a0c
Fernando Neto <fernandompneto@fernandompneto.(none)>
2013-02-19 22:46:27 -0300

    F

---
+++ forecast_pita_fernando/Te.java
@@ -0,0 +1,13 @@
+package logic_operacao2;
+
+public class Te {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println(11111111111111111111111111111111111111111111.1);
+	}
+
+}
